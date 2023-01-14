2ac8d112d6012aa6485c737c7a822f613543c376
Julienzer <julien.2001@hotmail.fr>
2023-01-14 16:15:39 +0100

    Modification de l'authentification, à adapter avec les fonctions de hash pour de la sécurité.

--- authenticate.php
+++ authenticate.php
@@ -1,38 +1,49 @@
 <?php
+//connect to the database
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_dbname";
 
-// Inclure la bibliothèque de JWT de Firebase
-require_once 'C:/Users/julie/vendor/firebase/php-jwt/src/JWT.php'; // à changer en fonction du dossier d'installation
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Clé secrète utilisée pour signer les JWT
-$secret_key = 'your_secret_key';
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-// Vérifier si une demande de connexion a été envoyée
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    // Récupérer les identifiants de connexion envoyés par le client
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//check if the login form has been submitted
+if (isset($_POST['submit'])) {
+    //get the login data
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Vérifier les identifiants de connexion (remplacez ceci par votre propre logique d'authentification)
-    if ($username == 'your_username' && $password == 'your_password') {
-        // Générer un JWT contenant les informations d'identification de l'utilisateur
-        $token = array(
-            'username' => $username,
-            'privilege' => 'admin', // Niveau de privilège de l'utilisateur (par exemple, admin ou user)
-        );
-        $jwt = JWT::encode($token, $secret_key);
+    //query the database to check if the user exists
+    $query = "SELECT * FROM user WHERE username = ? AND password = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
 
-        // Renvoyer le JWT au client de l'API
-        header('Content-type: application/json');
-        echo json_encode(array(
-            'jwt' => $jwt,
-        ));
+    //if the user exists
+    if ($user) {
+        //store the user data in a session
+        session_start();
+        $_SESSION['user'] = $user;
+
+        //redirect the user to the protected page
+        header("Location: protected.php");
     } else {
-        // Les identifiants de connexion sont incorrects, renvoyer une erreur
-        header('HTTP/1.1 401 Unauthorized');
-        echo 'Invalid username or password';
+        //if the user doesn't exist, show an error message
+        echo "Invalid username or password.";
     }
-} else {
-    // Aucune demande de connexion n'a été envoyée, renvoyer une erreur
-    header('HTTP/1.1 400 Bad Request');
-    echo 'Missing username or password';
 }
+
+$conn->close();
+?>
